5e04e4d23db6a1fe6616ac3cf49edafe7865bc78
jvsoftware2017 <victor_duarte12@yahoo.es>
2017-03-17 07:12:57 -0500

    Update clients.blade.php

--- resources/views/clients/clients.blade.php
+++ resources/views/clients/clients.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app') @section('title', 'Clientes | DriveSysMonitor')
+@extends('layouts.app') @section('title', 'Clientesss | DriveSysMonitor')
 
 @section('content')
 <!-- page content -->
