de308ee0d0a850ea67f49e2890c0ffb6c162beba
jose <jose@jose>
2017-03-23 20:27:15 -0500

    home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,9 +7,18 @@
             <div class="page-title">
                 <div class="title_left">
                     <h1>DriveSysMonitor</h1>
-                    <h4>Bienvenido, {{ Auth::user()->name}}</h4>
-                    <p>Tu licencia es válida hasta el: <b>{{ Auth::user()->client->dateValidity }}</b></p>
+                    <h2>Tu licencia es válida hasta el: <b>{{ Auth::user()->client->dateValidity }}</b></h2>
+                        <div class="x_panel">
+                          <div class="x_title">
+                            <h2>Cambiar password?</h2>
+                            <div class="clearfix"></div>
+                            <div class="x_content">
+                            <br />
+                            En la parte superior derecha, en el link de tu pertíl, puedes cambiar tu password.
+                            </div>
+                          </div>
 					{{ csrf_field() }}
+                </div>
                 </div>
             </div>
         </div>
