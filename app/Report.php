4782fb3d0b769c6b42798304b522f852c353f094
jvsoftware2017 <victor_duarte12@yahoo.es>
2018-04-10 07:21:07 -0500

    change report

--- app/Report.php
+++ app/Report.php
@@ -9,6 +9,6 @@ class Report extends Model
 	protected $table = 'reports';
 	
 	public static function getValuesToGraphic($variable, $id_equipo, $month, $anio){
-		return Report::where("variable", "=", $variable)->where("id_equipo", "=", $id_equipo)->whereYear('created_at', '=', $anio)->whereMonth('date', '=', $month)->get();
+		return Report::where("variable", "=", $variable)->where("id_equipo", "=", $id_equipo)->whereYear('date', '=', $anio)->whereMonth('date', '=', $month)->get();
 	}
 }
